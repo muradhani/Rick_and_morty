0eb65a20ebd3cee978028ca3b3e1b8aff3a69398
muradhani <mouradhany574@gmail.com>
2025-04-11 16:57:15 +0200

    ``` feat: Integrate Ktor client for network requests and add network module functionalities
    This commit introduces the Ktor client for handling network requests and enhances the `:network` module with essential functionalities.
    
    -   Added `KtorClient` class in the `:network` module to handle API requests, including a method to fetch character data.
    -   Implemented `Character` data class with inner `Origin` class to manage response data.
    -   Configured Ktor client in the `:network` module to use Android engine, logging, and content negotiation with JSON serialization.
    -   Updated dependencies in `libs.versions.toml` to include Ktor client libraries (core, Android, logging, content negotiation) and Kotlin serialization.
    -   Updated `build.gradle.kts` files to include network dependencies, content negotiation and kotlin serialization plugins.
    -   Added `network` module to app
    -   Added `kotlin serialization plugin`
    -   Added a test to check if the name of the first character is present in the app.
    ```

--- app/src/main/java/com/example/rickandmprty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmprty/MainActivity.kt
@@ -9,21 +9,39 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.network.Character
+import com.example.network.KtorClient
 import com.example.rickandmprty.ui.theme.RickAndMprtyTheme
 
 class MainActivity : ComponentActivity() {
+    private val ktorClient = KtorClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var character by remember {
+                mutableStateOf<Character?>(null)
+            }
+            val scope = rememberCoroutineScope()
+            LaunchedEffect(key1 = Unit, block = {
+                character = ktorClient.getCharacter(1)
+            })
             RickAndMprtyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
+                    Text(text = character?.name ?:"no name")
                 }
             }
         }

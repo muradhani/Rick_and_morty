9ca36e7408e87b742960c55fe188d9a1ab80b6c0
muradhani <mouradhany574@gmail.com>
2025-04-13 00:59:16 +0200

    ``` feat: Update CharacterStatusComponent for improved styling
    This commit refines the `CharacterStatusComponent` by updating its styling and layout.
    
    -   Adjusted the border and background shape of the component to use rounded corners with a radius of 10dp.
    -   Modified padding to use 10dp horizontally and 3dp vertically.
    -   Set the background color to transparent
    -   Added text color to white
    ```

--- app/src/main/java/com/example/rickandmprty/components/CharacterStatusComponent.kt
+++ app/src/main/java/com/example/rickandmprty/components/CharacterStatusComponent.kt
@@ -19,17 +19,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.domain.modules.CharacterStatus
+import com.example.rickandmprty.ui.theme.RickAction
 
 @Composable
 fun CharacterStatusComponent(characterStatus: CharacterStatus) {
     Row(
         modifier = Modifier.width(IntrinsicSize.Min)
-            .border(width = 1.dp, color = androidx.compose.ui.graphics.Color(characterStatus.color), shape = RoundedCornerShape(12.dp))
-            .background(color = androidx.compose.ui.graphics.Color(Color.TRANSPARENT), shape = RoundedCornerShape(12.dp))
-            .padding(top = 8.dp, bottom = 8.dp, start = 10.dp, end = 10.dp)
+            .border(width = 1.dp, color = androidx.compose.ui.graphics.Color(characterStatus.color), shape = RoundedCornerShape(10.dp))
+            .background(color = androidx.compose.ui.graphics.Color(Color.TRANSPARENT))
+            .padding(horizontal = 10.dp, vertical = 3.dp)
     ) {
-        Text(text = "Status: ", fontSize = 14.sp , modifier = Modifier.width(IntrinsicSize.Min))
-        Text(modifier = Modifier.width(IntrinsicSize.Min),text = characterStatus.displayName, fontSize = 14.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Status: ", fontSize = 14.sp , color = androidx.compose.ui.graphics.Color.White, modifier = Modifier.width(IntrinsicSize.Min))
+        Text(modifier = Modifier.width(IntrinsicSize.Min),text = characterStatus.displayName, fontSize = 14.sp, fontWeight = FontWeight.Bold, color = androidx.compose.ui.graphics.Color.White)
     }
 
 }

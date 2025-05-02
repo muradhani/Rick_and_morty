b73dd875193b1f557aaadb8e5d8b6d7768699cc3
muradhani <mouradhany574@gmail.com>
2025-05-02 20:24:38 +0300

    Add EpisodesScreenViewModel and GetEpisodesUseCase

---
+++ app/src/main/java/com/example/rickandmprty/viewmodels/EpisodesScreenViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.rickandmprty.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.modules.Episode
+import com.example.domain.useCases.GetCharacterUseCase
+import com.example.domain.useCases.GetEpisodesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EpisodesScreenViewModel @Inject constructor(
+    private val getEpisodesUseCase: GetEpisodesUseCase
+): ViewModel() {
+    private var _episodes = MutableStateFlow<List<Episode>?>(null)
+    var episodes: StateFlow<List<Episode>?> = _episodes
+    fun getEpisodes(episodeIds: List<String>){
+        viewModelScope.launch {
+            _episodes.value = getEpisodesUseCase(extractEpsiodesIds(episodeIds))
+
+        }
+    }
+    fun extractEpsiodesIds(episodeUrls: List<String>): String {
+        return episodeUrls.joinToString(",") { it.substringAfterLast("/") }
+    }
+}

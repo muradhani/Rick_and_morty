eec7316efcbee24ef597dafd0cd43b55bf9aea11
muradhani <mouradhany574@gmail.com>
2025-05-02 21:17:21 +0300

    Remove unused state variables and LaunchedEffect in CharacterEpisodeScreen

--- app/src/main/java/com/example/rickandmprty/screens/CharacterEpisodeScreen.kt
+++ app/src/main/java/com/example/rickandmprty/screens/CharacterEpisodeScreen.kt
@@ -15,15 +15,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle

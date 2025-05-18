b8833deb4c6ab089fa6425ffb100bfd9a378c083
muradhani <mouradhany574@gmail.com>
2025-05-18 23:47:50 +0300

    Fetch characters in HomeScreen
    This commit introduces the fetching of character data in the HomeScreen.
    
    Key changes:
    - Injected `HomeScreenViewModel` into `HomeScreen`.
    - Collected characters from the view model using `collectAsStateWithLifecycle`.
    - Set a background color for the `HomeScreen`.
    - In `HomeScreenViewModel`:
        - Added `getCharacterPaging` use case.
        - Introduced `_characters` MutableStateFlow to hold character data.
        - Exposed `characters` as a StateFlow.

--- app/src/main/java/com/example/rickandmprty/screens/HomeScreen.kt
+++ app/src/main/java/com/example/rickandmprty/screens/HomeScreen.kt
@@ -1,14 +1,23 @@
 package com.example.rickandmprty.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.rickandmprty.ui.theme.RickPrimary
+import com.example.rickandmprty.viewmodels.HomeScreenViewModel
 
 @Composable
 fun HomeScreen(
-
+    viewModel: HomeScreenViewModel = hiltViewModel()
 ) {
-    Column(
-
+    val character = viewModel.characters.collectAsStateWithLifecycle().value
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = RickPrimary
     ) {
 
     }
--- app/src/main/java/com/example/rickandmprty/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/rickandmprty/viewmodels/HomeScreenViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.rickandmprty.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.domain.modules.Character
+import com.example.domain.useCases.getCharacterPaging
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeScreenViewModel @Inject constructor(
-
+    private val getCharactersPaging: getCharacterPaging
 ) : ViewModel() {
+    private var _characters = MutableStateFlow<List<Character>?>(null)
+    var characters: StateFlow<List<Character>?> = _characters
 }

a30802a53f837cc6a1f7eff4644ab873039cdcd4
muradhani <mouradhany574@gmail.com>
2025-05-20 10:53:26 +0300

    Implement lazy loading grid for characters in HomeScreen
    This commit introduces a `LazyVerticalGrid` to display characters in the `HomeScreen`
    
    Key changes:
    - Replaced `Column` with `LazyVerticalGrid` in `HomeScreen`.
    - Used `rememberLazyGridState` to manage the grid's scroll state.
    - Fetched initial character data using `LaunchedEffect` when the screen is first composed.
    - Implemented `derivedStateOf` to detect when the user has scrolled near the end of the character list.
    - Added a `LaunchedEffect` to trigger fetching the next page of characters when `loadNextPage` becomes true.
    - Displayed each character using a `CharacterListItem` composable within the grid.

--- app/src/main/java/com/example/rickandmprty/screens/HomeScreen.kt
+++ app/src/main/java/com/example/rickandmprty/screens/HomeScreen.kt
@@ -1,29 +1,56 @@
 package com.example.rickandmprty.screens
 
-import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.rickandmprty.ui.theme.RickPrimary
 import com.example.rickandmprty.viewmodels.HomeScreenViewModel
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.foundation.lazy.grid.items
+import com.example.rickandmprty.components.CharacterListItem
+
 
 @Composable
 fun HomeScreen(
     viewModel: HomeScreenViewModel = hiltViewModel()
 ) {
-    val character = viewModel.characters.collectAsStateWithLifecycle().value
+    val characters = viewModel.characters.collectAsStateWithLifecycle().value
+    LaunchedEffect(key1 = viewModel, block = {viewModel.getCharacters(1)})
+    val scrollStateCharacters = rememberLazyGridState()
+    var loadNextPage = remember {
+        derivedStateOf {
+            val visibleItemPosition = scrollStateCharacters.layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: return@derivedStateOf false
+            val charactersCount = characters?.size ?: return@derivedStateOf false
+            visibleItemPosition >= charactersCount - 10
+        }
+    }
+    LaunchedEffect(key1 = loadNextPage, block = {
+        if (loadNextPage.value) viewModel.getNextCharactersPage()
+    })
 
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = RickPrimary
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-
+        characters?.let { characters ->
+            LazyVerticalGrid(
+                modifier = Modifier.fillMaxSize(),
+                state = scrollStateCharacters,
+                columns = GridCells.Fixed(2),
+            ){
+                items(characters) { character ->
+                    CharacterListItem(character = character , onClick = {})
+                }
+            }
         }
     }
 }
